2607a9f55ec02d372723e90fe2ccaf18d7bc044b
Luiz Felipe Royer Pereira <royer.felippe@gmail.com>
2025-01-09 21:18:37 -0300

    change index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home(){
-    return <h1> Só passei para te dizer que TE AMO ... muito mais </h1>
+    return <h1>
+        <h2>Oi Gatinha</h2>
+         Só passei para te dizer que TE AMO ...
+        
+         muito mais 
+         
+         
+         </h1>
 }
 
 export default Home
