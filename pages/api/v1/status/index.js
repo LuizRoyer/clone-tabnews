556b6b03c5a3afedc0e2c83552441ff8a5f68bc2
Luiz Felipe Royer Pereira <royer.felippe@gmail.com>
2025-02-14 21:40:23 -0300

    change get status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,25 @@
-import database from "../../../../api/configurations/database.js"
+import database from "/api/configurations/database.js"
 
 export default async function status(req, res) {
-    const result = await database.query("select 2+2*2 as sum;")
-    console.log(result.rows)
-    res.status(200).json({ "status": 200, "message": "" })
+    const updateAt = new Date().toISOString()
+
+    const version = await database.query("show server_version;")
+    const maxConnection = await database.query("show  max_connections;")
+    const openedConnection = await database.query({
+        text: "select count(*)::int from pg_stat_activity where datname = $1;",
+        values: [process.env.POSTGRES_DB]
+    })
+
+    const dependencies = {
+        database: {
+            version: version.rows[0].server_version,
+            max_connection: parseInt(maxConnection.rows[0].max_connections),
+            opened_connection: openedConnection.rows[0].count
+        }
+    }
+    res.status(200).json({
+        status: 200,
+        updated_at: updateAt,
+        dependencies
+    })
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,18 @@
+const url = "http://localhost:3000/api/v1/status"
+
 test("Get to api/v1/status should return 200", async () => {
-    const res = await fetch("http://localhost:3000/api/v1/status")
+    const res = await fetch(url)
 
     expect(res.status).toBe(200);
+
+
+    const body = await res.json()
+    expect(body.updated_at).toBeDefined();
+    expect(body.updated_at).toEqual(new Date(body.updated_at).toISOString())
+
+    expect(body.dependencies.database.version).toEqual("16.0")
+    expect(body.dependencies.database.max_connection).toBeDefined();
+    expect(body.dependencies.database.max_connection).toEqual(100)
+    expect(body.dependencies.database.opened_connection).toEqual(1)
 
 })
